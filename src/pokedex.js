e1a0c40abf32e097442df1fb1759bb4a59ab7518
alinepasetti <pasettialine@gmail.com>
2020-07-14 18:50:52 +0100

    ajuste nos comentários

--- src/pokedex.js
+++ src/pokedex.js
@@ -2,13 +2,13 @@
 
 class Ash {
   constructor() {
-    // a Pokedex tem como valor um número, que se refere aos Pokemons que o Ash coleta por vez. Este número será o output final da método final
+    // a Pokedex tem como valor um número, que se refere aos Pokemons que o Ash coleta com os seus movimentos. Este número será o output final do método collectingPokemons
     this.pokedex = 0;
 
     // a current position altera conforme o Ash se move, ela sempre carrega apenas um objeto com as coordenadas x e y, que é onde o Ash está posicionado naquele momento
     this.currentPosition = { x: 0, y: 0 };
 
-    // position log vai estocar em uma array todas as posições que o Ash visitou na grid. Esta array será o suporte principal para o método que verifica se há pokemon na casa atual em que Ash se encontra ou não
+    // position log vai estocar em uma array todas as posições que o Ash visitou na grid. Esta array será o suporte principal para o método chekingIfThereIsAPokemon, que verifica se há ou não um Pokemon na casa atual em que Ash se encontra
     this.positionLog = [];
   }
 
@@ -24,34 +24,30 @@ class Ash {
     switch (coordinate.toUpperCase()) {
       case 'S':
         this.currentPosition.y++;
-        // console.log(this.currentPosition)
         break;
       case 'N':
         this.currentPosition.y--;
-        // console.log(this.currentPosition)
         break;
       case 'O':
         this.currentPosition.x--;
-        // console.log(this.currentPosition)
         break;
       case 'E':
         this.currentPosition.x++;
-        // console.log(this.currentPosition)
         break;
-      // quando o input utilizado é inválido pois não se trata de nenhum dos caracteres previstos, um novo erro é thrown
+      // quando o input utilizado é inválido, pois não se trata de nenhum dos caracteres previstos, um novo erro é thrown
       default:
         throw new Error(`Input ${coordinate} is invalid. Please insert either S, N, E or O`);
     }
   }
 
-  // Método que verifica se há um Pokemon naquele bloco ou não. Ele compara o positionLog com a current position
+  // Método: verifica se há um Pokemon naquele bloco ou não. Ele compara as posições visitadas no positionLog com a current position
   chekingIfThereIsAPokemon() {
     if (
       !this.positionLog.some(
         (position) => position.x === this.currentPosition.x && position.y === this.currentPosition.y
       )
     ) {
-      console.log(this.currentPosition);
+      // caso a casa não tenha sido visitada, ele adiciona um Pokemon na pokedex
       this.pokedex++;
     }
   }
@@ -62,20 +58,20 @@ class Ash {
     if (!coordinates || !coordinates.length || typeof coordinates !== 'string') {
       throw new Error('You must send coordinates to Ash. Please insert either S, N, E or O');
     } else {
-      // chama o método que verifica se aquelas coordenadas já foram visitadas pelo Ash
+      // chama o método que verifica se aquelas coordenadas já foram visitadas pelo Ash e adiciona o Pokemon à Pokedex, caso faça sentido
       this.chekingIfThereIsAPokemon();
 
-      // chama o método para salvar a current position no log de positions é chamado antes de alterar a current position do Ash
+      // chama o método para salvar a current position no log de positions, é chamado antes de alterar a current position do Ash
       this.savePositionToPositionLog();
 
       // faz um loop nas coordenadas e separadamente chama o método que move o Ash pelas casas de acordo com a coordenada
       coordinates.split('').forEach((coordinate) => {
         this.movingAsh(coordinate);
 
-        // chama o método que verifica se aquelas coordenadas já foram visitadas pelo Ash
+        // chama o método que verifica se aquelas coordenadas já foram visitadas pelo Ash e adiciona o Pokemon à Pokedex, caso faça sentido
         this.chekingIfThereIsAPokemon();
 
-        // chama o método para salvar a current position no log de positions é chamado antes de alterar a current position do Ash
+        // chama o método para salvar a current position no log de positions
         this.savePositionToPositionLog();
       });
     }
